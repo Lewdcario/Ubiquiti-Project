ce4055527a5504b944fcc991f3768df386c8646b
Lewdcario <isabellafj97@gmail.com>
2023-08-30 06:56:02 +0000

    Deploying to gh-pages from @ Lewdcario/Ubiquiti-Project@d3236b97274a1101df00dcd79537ec5be97420d1 🚀

---
+++ _next/static/vvmZegyVkpDonPSJ5GtqH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-afe5c819510f654f.js","static/chunks/pages/index-9e0f7c5ecd3edb86.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
