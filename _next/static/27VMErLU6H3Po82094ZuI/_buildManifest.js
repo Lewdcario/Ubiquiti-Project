01dbab29714bf929ef5a416dd5a9f07474c63023
Lewdcario <isabellafj97@gmail.com>
2023-09-03 21:23:41 +0000

    Deploying to gh-pages from @ Lewdcario/Ubiquiti-Project@4f503247e147b6cbe0f24d309ef068d6cd53249d 🚀

---
+++ _next/static/27VMErLU6H3Po82094ZuI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-332ec9e845dfb4a0.js","static/chunks/pages/index-a9854b269ae4dae0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/sHVcJFvg-ilny207d74ud/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-afe5c819510f654f.js","static/chunks/pages/index-f1a9c12455d2b2df.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
